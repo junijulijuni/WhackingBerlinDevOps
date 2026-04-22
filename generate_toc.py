5af77639a34c4ce9971fddfddde8946701a77a98
whackwhack <kathol-voilleque@campus.tu-berlin.de>
2026-04-22 11:44:25 +0200

    Update generate_toc.py

--- generate_toc.py
+++ generate_toc.py
@@ -50,25 +50,8 @@ def main():
         f.write(new_readme)
 
 if __name__ == "__main__":
-    main()import os
-
-def generate_toc(path, indent=0):
-    toc = []
-    for item in sorted(os.listdir(path)):
-        item_path = os.path.join(path, item)
-        if os.path.isdir(item_path):
-            if item.startswith('.'):
-                continue
-            toc.append("  " * indent + f"- [{item}]({item})")
-            toc.extend(generate_toc(item_path, indent + 1))
-        elif item.endswith('.md') and item != 'README.md':
-            toc.append("  " * indent + f"- [{item[:-3]}]({item})")
-    return toc
-
-def main():
-    toc = ["# Whacking Infodump", "", "## Table of Contents", ""] + generate_toc('.')
-    with open('README.md', 'w') as f:
-        f.write('\n'.join(toc))
-
-if __name__ == "__main__":
     main()
+
+
+
+
